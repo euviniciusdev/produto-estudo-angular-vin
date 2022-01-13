5c4f6b50feb8b1950b0a7e9b4970aef428b2fd72
euviniciusdev <josevinicius.vin@gmail.com>
2022-01-12 21:28:55 -0300

    Adição do projeto no repositório GitHub

---
+++ src/app/exemplos/exemplos-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+
+import { CriacaoPipesComponent } from './criacao-pipes/criacao-pipes.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DiretivasComponent } from './diretivas/diretivas.component';
+import { PipesComponent } from './pipes/pipes.component';
+
+const routes: Route[] = [
+  {
+    path: 'data-binding',
+    component: DataBindingComponent,
+  },
+  {
+    path: 'diretivas',
+    component: DiretivasComponent,
+  },
+  {
+    path: 'pipes',
+    component: PipesComponent,
+  },
+  {
+    path: 'criacao-pipes',
+    component: CriacaoPipesComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ExemplosRoutingModule {}
---
+++ src/app/exemplos/exemplos.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { CompartilhadoModule } from './../compartilhado/compartilhado.module';
+import { CriacaoPipesComponent } from './criacao-pipes/criacao-pipes.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DiretivasComponent } from './diretivas/diretivas.component';
+import { ExemplosRoutingModule } from './exemplos-routing.module';
+import { PipesComponent } from './pipes/pipes.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    DataBindingComponent,
+    DiretivasComponent,
+    PipesComponent,
+    CriacaoPipesComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ExemplosRoutingModule,
+    CompartilhadoModule
+  ],
+  exports: [
+    DataBindingComponent
+  ]
+})
+export class ExemplosModule { }
